6f3e41a6666f1d24244cdf6b0afe2b2ca4b6efc7
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-10-10 21:07:54 +0200

    benign test modifications

--- tests/testthat/test-align.R
+++ tests/testthat/test-align.R
@@ -3,7 +3,8 @@ load("CH07.rda")
 # Check on a square and a non-square map
 map_list <- list(
   `square` = CH07,
-  `non-square` = crop_map(CH07, data.frame(ll.lat = 45.68, ur.lat = 47.9, ll.lon = 5.79, ur.lon = 10.7)))
+  `non-square` = crop_map(CH07, data.frame(ll.lat = 45.68, ur.lat = 47.9, ll.lon = 5.79, ur.lon = 10.7))
+)
 
 lapply(
   names(map_list),
@@ -57,39 +58,33 @@ lapply(
       bb_pix <- align_bb_to_pixels(map, bb)$pixels
       expect_equal(bb_pix, data.frame(ll.x = 1, ll.y = d[[1]], ur.x = d[[2]], ur.y = 1))
 
-      check_unchanged <- function(bb_new) {
+      check_unchanged <- function(bb_new, info) {
         aligned <- align_bb_to_pixels(map, bb_new)
-        expect_equal(aligned$pixels, bb_pix)
-        expect_equal(aligned$corrected, bb)
+        expect_equal(aligned$pixels, bb_pix, info = info)
+        expect_equal(aligned$corrected, bb, info = info)
       }
 
-      check_unchanged(bb)
+      check_unchanged(bb, "Identity")
 
       epsilon = 2 ** -16
 
-      # Move left border almost one pixel to the right
       bb_new <- transform(bb, ll.lon = ll.lon + (1 - epsilon) / d[[2]] * (ur.lon - ll.lon))
-      check_unchanged(bb_new)
+      check_unchanged(bb_new, "Move left border almost one pixel to the right")
 
-      # Move right border almost one pixel to the left
       bb_new <- transform(bb, ur.lon = ur.lon - (1 - epsilon) / d[[2]] * (ur.lon - ll.lon))
-      check_unchanged(bb_new)
+      check_unchanged(bb_new, "Move right border almost one pixel to the left")
 
-      # Move top border almost one pixel to the bottom
       bb_new <- transform(bb, ur.lat = ur.lat - (1 - epsilon) / d[[1]] * (ur.lat - ll.lat))
-      check_unchanged(bb_new)
+      check_unchanged(bb_new, "Move top border almost one pixel to the bottom")
 
-      # Move bottom border almost one pixel to the top
       bb_new <- transform(bb, ll.lat = ll.lat + (1 - epsilon) / d[[1]] * (ur.lat - ll.lat))
-      check_unchanged(bb_new)
+      check_unchanged(bb_new, "Move bottom border almost one pixel to the top")
 
-      # Can mirror horizontally
       bb_new <- transform(bb, ll.lat = ur.lat, ur.lat = ll.lat)
-      check_unchanged(bb_new)
+      check_unchanged(bb_new, "Can mirror horizontally")
 
-      # Can mirror vertically
       bb_new <- transform(bb, ll.lon = ur.lon, ur.lon = ll.lon)
-      check_unchanged(bb_new)
+      check_unchanged(bb_new, "Can mirror vertically")
     })
 
     test_that("Can't enlarge bounding box", {
