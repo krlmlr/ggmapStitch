0c370af87f5728dc01865d22dc7288a73acd3456
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-10-10 09:56:25 +0200

    stub for crop_map

---
+++ R/crop.R
@@ -0,0 +1,24 @@
+#' Crops a map to a new bounding box
+#'
+#' This function reduces the dimensions of a map to fit a given bounding box
+#' as closely as possible.  The bounding box is readjusted to fit pixel
+#' boundaries.
+#'
+#' @export
+crop_map <- function(map, new_bb) {
+  stop("NYI")
+  new_bb_pix <- ll2pix(map, new_bb)
+  new_bb_pix_ll <- pix2ll(map, new_bb_pix)
+
+  stopifnot(new_bb_pix_ll$ll.lat <= new_bb$ll.lat)
+  stopifnot(new_bb_pix_ll$ur.lat >= new_bb$ur.lat)
+  stopifnot(new_bb_pix_ll$ll.lon <= new_bb$ll.lon)
+  stopifnot(new_bb_pix_ll$ur.lon >= new_bb$ur.lon)
+
+  structure(
+    map[seq(from = new_bb_pix$ur.y, to = new_bb_pix$ll.y, by = 1),
+        seq(from = new_bb_pix$ll.x, to = new_bb_pix$ur.x, by = 1)],
+    class = class(map),
+    bb = new_bb_pix_ll
+  )
+}
