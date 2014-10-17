6bf00044630cbba1cff51be9e50941490b62abf2
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-10-17 09:44:26 +0200

    add example for stitch_map

--- R/ggmapStitch-package.r
+++ R/ggmapStitch-package.r
@@ -1,4 +1,4 @@
-#' ggmapStitch.
+#' Arbitrarily large maps with ggmap
 #'
 #' The ggmap package offers downloading map tiles from a variety of
 #' providers, but (at least for Google Maps) the maps are restricted to a maximum
@@ -7,4 +7,16 @@
 #'
 #' @name ggmapStitch
 #' @docType package
+#' @examples
+#' require(ggmap)
+#'
+#' bb_ch <- data.frame(ll.lat = 45.68, ur.lat = 47.9,
+#'                     ll.lon = 5.79, ur.lon = 10.7)
+#'
+#' # Zoom level 8 is a good choice, but default ggmap() will clip away Geneva
+#' ggmap(stitch_map(bb = bb_ch, location = "Switzerland", zoom = 8))
+#'
+#' \dontrun{
+#' # At zoom level 9, about 10 tiles will be downloaded
+#' ggmap(stitch_map(bb = bb_ch, location = "Switzerland", zoom = 9))}
 NULL
